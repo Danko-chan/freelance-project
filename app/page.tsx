58602f1e00009479ad8a7da3017284bfd812be97
Abhay Verma <abhayverma490@gmail.com>
2024-08-27 18:45:13 +0530

    inti

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,95 @@
 import Image from "next/image";
+import building from "@/public/2.jpg"
+import Triange from "@/public/triangle.svg"
+import logo from "@/public/vercel.svg"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+   
+      <div className="content w-full h-svh">
+        <header>
+          <nav className="">
+            <ul className="flex mx-[5%] h-[147px] items-center">
+              <li className="w-[15%] border-r justify-center items-center flex border-black">
+              <Image 
+              src={logo}
+              alt="1"
+              width={130}
+              height={130}
             />
-          </a>
+              </li>
+              <li className="w-[15%] text-center text-3xl">Something</li>
+              <li className="w-[auto] flex-grow text-right"><p><span className="text-xs font-bold">Tél.</span> <Link href="#" className="text-xl">+00 00 000 00 00</Link><span className="text-xs font-bold">&nbsp;&nbsp;&nbsp; Mail.</span> 
+              <Link href="#" className="text-xl pl-1">something@some.com</Link></p></li>
+              <li className="w-[10%] justify-center items-center flex">
+                <div className="w-12 h-12 border border-black rounded-full flex justify-center items-center transform transition-transform duration-300 ease-in hover:rotate-90">
+                  <div>
+                  <div className="w-4 border-b border-black m-1" />
+                  <div className="w-4 border-b-2 border-gray-400 m-1" />
+                  <div className="w-4 border-b border-black m-1" />
+                  </div>
+                  
+                </div>
+              </li>
+            </ul> 
+          </nav>
+        </header>
+        <section className="pb-12">
+        <div className="flex h-svh w-full">
+          <div className=" relative basis-[60%] ">
+            <Image 
+              src={building}
+              alt="1"
+              fill={true}
+            />
+            <div className="absolute inset-0 flex justify-start  items-center">
+                <div className="content border uppercase bg-white rounded-r-full p-4">
+                 <p className="border pl-10 pr-4 py-2 font-extralight border-black rounded-r-full -ml-8"><b className="font-bold">plus que 1 appartement disponible</b><br/>chantier en cours &gt; Livraison 09/2024<br/>visite possible sur rendez-vous</p>
+                </div>
+            </div> 
+            <div className="absolute inset-0 flex justify-end transform translate-x-4 items-center">
+                <div className="content border w-32 h-32 flex justify-center items-center rounded-full bg-white p-4">
+                  <div className="border border-black p-8 rounded-full">
+                  <Image
+                  src={Triange}
+                  alt="triange"
+                  width={20}
+                  height={20}
+                  className="transform rotate-90"
+                  /></div>
+                </div>
+            </div> 
+          </div>
+          <div className="basis-[20%]  px-8">
+            <div className="relative w-full h-1/3 bg-gray-300">
+            </div>
+            <div className="flex justify-end mt-24 text-right">
+                <div>
+                <p className="uppercase font-extralight"><b className="font-bold">14 appartements</b><br/>ensoleillés au coeur<br/>de l’agglomération<br/>lausannoise</p>
+              <div className="flex flex-col items-end space-y-2 my-12 mr-4">
+               <div className="border-r border-black h-32 "></div>
+               <hr className="w-8 absolute transform translate-x-4 translate-y-[120px] border-black"/>
+                </div>
+                <p className="uppercase font-thin text-lg text-green-400">Permis en force et exécutoire</p>
+              </div>
+            </div>
+            <div className="button w-full uppercase font-extralight text-center border border-black py-2 mt-12"> Situation</div>
+            {/* <p class="para-right"><img src="svg/deco-ligne2.svg" width="30px"></p>
+            
+            <a class="bouton-full" href="https://goo.gl/maps/esvvi9sCCZ3GnJtM6" target="blank"><p>situation</p></a> */}
+          </div>
+          <div className=" relative basis-[20%]">
+            <Image
+            src={building}
+            alt="building"
+            fill={true}
+            />
+          </div>
         </div>
+
+        </section>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
